b90ccdf0dcd4beaee5be45c2b948b5bfb8d89021
AsueDerick <asuederipotas7@gmail.com>
2025-07-23 05:45:37 +0400

    configured ami and instance with the necessary output values

--- main.tf
+++ main.tf
@@ -66,21 +66,65 @@ resource "aws_security_group" "myapp_sg" {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = "0.0.0.0/0" # Allow HTTP from anywhere consider restricting this in production
+        cidr_blocks =["0.0.0.0/0"] # Allow HTTP from anywhere consider restricting this in production
     }
     ingress {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = "0.0.0.0/0" # Allow SSH from anywhere, consider restricting this in production
+        cidr_blocks =["0.0.0.0/0"]# Allow SSH from anywhere, consider restricting this in production
     }
     egress {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
-        cidr_blocks = ""
+        cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic consider restricting this in production
     }
     tags = {
         Name = "${var.env_prefix}-security-group"
     }
 }
+
+data "aws_ami" "myapp_ami" {
+  most_recent = true
+  owners      = ["amazon"] # Use Amazon's official AMIs
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"] # Example for Amazon Linux 2, adjust as needed
+  }    
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]     
+  
+} 
+}
+
+resource "aws_instance" "myapp_instance" {
+  ami           = data.aws_ami.myapp_ami.id # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.myapp_subnet.id
+  vpc_security_group_ids = [aws_security_group.myapp_sg.id]
+    tags = {
+        Name = "${var.env_prefix}-instance"
+    }
+}
+output "vpc_id" {    
+  value = aws_vpc.myapp_vpc.id    # Output the VPC ID  
+}
+output "subnet_id" {
+  value = aws_subnet.myapp_subnet.id # Output the Subnet ID
+}
+output "instance_id" {
+  value = aws_instance.myapp_instance.id # Output the Instance ID
+} 
+output "myapp_ami_id" {
+  value = data.aws_ami.myapp_ami.id # Output the AMI ID
+}      
+output "instance_public_ip" {
+  value =  aws_instance.myapp_instance.public_ip # Output the Instance Public IP
+}    
+
