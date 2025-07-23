34bed681e1c8a4dde8c667604482d1f1a2a43a72
AsueDerick <asuederipotas7@gmail.com>
2025-07-23 04:23:20 +0400

    configuring internet gateway and route table

--- main.tf
+++ main.tf
@@ -22,17 +22,36 @@ variable "env_prefix" {
   type        = string 
   
 }
-resource "aws_vpc" "main" {
+resource "aws_vpc" "myapp_vpc" {
   cidr_block = var.vpc_cdr_block
     tags = {
         Name = "${var.env_prefix}-vpc"
     }
 }
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
+resource "aws_subnet" "myapp_subnet" {
+  vpc_id            = aws_vpc.myapp_vpc.id
   cidr_block        = var.subnet_cidr_blocks
   availability_zone = var.avail_zone
     tags = {
-        Name = "${var.env_prefix}-subnet"
+        Name = "${var.env_prefix}-subnet-1"
     }
 }
+
+resource "aws_internet_gateway" "myapp_igw" {
+  vpc_id = aws_vpc.myapp_vpc.id
+    tags = {
+        Name = "${var.env_prefix}-internet-gateway"
+    }
+}
+
+resource "aws_route_table" "myapp_route_table" {
+  vpc_id = aws_vpc.myapp_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp_igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-route-table"
+    }
+}
+
